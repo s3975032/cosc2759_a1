3be47cdce36fa44e667de9aae80e8d340b2a7c9c
Anhtu <s3975032@rmit.edu.vn>
2024-08-14 17:55:39 +1000

    fix problem for testing

--- src/tests/integration/note.test.js
+++ src/tests/integration/note.test.js
@@ -1,21 +1,18 @@
-const app = require('../../app')
-const supertest = require('supertest')
-const req = supertest(app)
-const Note = require("../../models/note")
+const app = require("../../app");
+const supertest = require("supertest");
+const req = supertest(app);
+// const Note = require("../../models/note")
 
 // Integration Tests programmatically test the API used by the application
 
-describe('Add Note', () => {
-    it('Should add a new note and redirect to login', async () => {
+describe("Add Note", () => {
+  it("Should add a new note and redirect to login", async () => {
+    const res = await req.post("/").send({
+      title: "[INTEGRATION TEST] New Note",
+      description: "This note was created at " + new Date(),
+    });
 
-        const res = await req
-            .post('/')
-            .send({
-                title: "[INTEGRATION TEST] New Note",
-                description: "This note was created at " + new Date(),
-            })
-
-        expect(res.statusCode).toEqual(302)
-        expect(res.headers['location']).toEqual('/')
-    })
-})
+    expect(res.statusCode).toEqual(302);
+    expect(res.headers["location"]).toEqual("/");
+  });
+});
